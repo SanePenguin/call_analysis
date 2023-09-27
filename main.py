8fdf538bb313d2dd097397bc0a05e856f5b1724d
René Delion <rene.leon.delion@gmail.com>
2023-09-28 00:36:40 +0200

    Change conversion to mp3 to wav

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import speech_recognition
 from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
 
-def convertRecordingsToMP3(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
+def convertRecordingsToWAV(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
     DIR_PATH = "."
     MP4FOLDER = DIR_PATH + MP4FOLDER
     MP3FOLDER = DIR_PATH + MP3FOLDER
@@ -9,11 +11,12 @@ def convertRecordingsToMP3(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
     for i, file in enumerate(os.listdir(MP4FOLDER)):
         clip = VideoFileClip(MP4FOLDER+"/"+file)
         audioclip = clip.audio
-        audioclip.write_audiofile(f"{MP3FOLDER}/audio{i}.mp3")
+        audioclip.write_audiofile(f"{MP3FOLDER}/audio{i}.wav")
         audioclip.close()
         clip.close()
 
 
 
+
 if __name__ == "__main__":
-    convertRecordingsToMP3()
+    convertRecordingsToWAV()
