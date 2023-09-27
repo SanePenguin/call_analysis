c220d56506099e17d5f9e62fbe67a1b4135f6e2b
René Delion <rene.leon.delion@gmail.com>
2023-09-28 01:07:21 +0200

    Update convertMP4toWAV method to be more general

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import os
 import speech_recognition as sr
 from moviepy.editor import VideoFileClip
 
-def convertRecordingsToWAV(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
-    DIR_PATH = "."
-    MP4FOLDER = DIR_PATH + MP4FOLDER
-    MP3FOLDER = DIR_PATH + MP3FOLDER
+def convertMP4toWAV(file, output_folder=".", file_name=None):
+    """Converts a mp4 file to WAV and saves it in the given folder
 
-    for i, file in enumerate(os.listdir(MP4FOLDER)):
-        clip = VideoFileClip(MP4FOLDER+"/"+file)
-        audioclip = clip.audio
-        audioclip.write_audiofile(f"{MP3FOLDER}/audio{i}.wav")
-        audioclip.close()
-        clip.close()
+    ARGS:
+        file: The file you want to convert
+        output_folder(path): the folder the file will be saved
+        filename(string): the name of the wav file
+    """
+    name = f"{os.path.splitext(os.path.basename(file))[0]}" if file_name==None else file_name
 
+    clip = VideoFileClip(file)
+    audioclip = clip.audio
+    audioclip.write_audiofile(f"{output_folder}/{name}.wav")
+    audioclip.close()
+    clip.close()
 
 def transcribeAudio(WAV_FILE, language="de"):
+    """Returns the transcribed wav of audio file as a string.
+
+    ARGS:
+        WAV_FILE: The audio file you want to transcribe
+        language: The languanges abbreviation of the languages used in the audio file
+    
+    """
     r = sr.Recognizer()
     with sr.AudioFile(WAV_FILE) as source:
         audio = r.listen(source)
@@ -24,5 +34,6 @@ def transcribeAudio(WAV_FILE, language="de"):
 
 
 if __name__ == "__main__":
-    convertRecordingsToWAV()
-    print(transcribeAudio(r'her_audios\audio0.wav'))
+    #convertRecordingsToWAV()
+    #print(transcribeAudio(r'her_audios\audio0.wav'))
+    convertMP4toWAV(r"recordings\sample_rec - Kopie.mp4", output_folder="./her_audios", file_name="gay")
