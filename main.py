7686efe6b8e9ae40da00398619d9449f718363e2
René Delion <rene.leon.delion@gmail.com>
2023-09-28 00:46:16 +0200

    Add function to transcribe audio

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-import speech_recognition
+import speech_recognition as sr
 from moviepy.editor import VideoFileClip
-from pydub import AudioSegment
 
 def convertRecordingsToWAV(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
     DIR_PATH = "."
@@ -16,7 +15,14 @@ def convertRecordingsToWAV(MP4FOLDER="/recordings", MP3FOLDER="/her_audios"):
         clip.close()
 
 
+def transcribeAudio(WAV_FILE, language="de"):
+    r = sr.Recognizer()
+    with sr.AudioFile(WAV_FILE) as source:
+        audio = r.listen(source)
+        return r.recognize_google(audio, language=language)
+
 
 
 if __name__ == "__main__":
     convertRecordingsToWAV()
+    print(transcribeAudio(r'her_audios\audio0.wav'))
